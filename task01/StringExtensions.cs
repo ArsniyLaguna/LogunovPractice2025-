a454858501e8d1f042c3f179cd2b99c3cd9911b2
АрсенийЛагуна <logunov7v@gmail.com>
2025-06-25 23:20:24 +0600

    Задание 1: метод IsPalindrome и тесты

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            var cleaned = new string(input
+                .Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                .Select(char.ToLower)
+                .ToArray());
+
+            var reversed = new string(cleaned.Reverse().ToArray());
+
+            return cleaned == reversed;
+        }
+    }
+}
