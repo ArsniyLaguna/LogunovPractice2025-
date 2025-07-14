a4ce7e71530fe1d4d195b12e9b8eb714c9959c08
АрсенийЛагуна <logunov7v@gmail.com>
2025-07-14 11:52:27 +0600

    Задание 10: Система плагинов

---
+++ PluginLoader/Program.cs
@@ -0,0 +1,36 @@
+﻿using PluginContracts;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+string pluginPath = args.Length > 0 ? args[0] : "./plugins";
+
+if (!Directory.Exists(pluginPath))
+{
+    Console.WriteLine($"Папка с плагинами не найдена: {pluginPath}");
+    return;
+}
+
+var dlls = Directory.GetFiles(pluginPath, "*.dll");
+var pluginTypes = new Dictionary<string, (Type type, PluginLoadAttribute attr)>();
+
+foreach (var dll in dlls)
+{
+    var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.GetFullPath(dll));
+
+    foreach (var type in assembly.GetTypes())
+    {
+        if (typeof(IPlugin).IsAssignableFrom(type) && type.GetCustomAttribute<PluginLoadAttribute>() is { } attr)
+        {
+            pluginTypes[type.FullName!] = (type, attr);
+        }
+    }
+}
+
+foreach (var (type, _) in pluginTypes.Values)
+{
+    if (Activator.CreateInstance(type) is IPlugin plugin)
+    {
+        plugin.Execute();
+    }
+}
